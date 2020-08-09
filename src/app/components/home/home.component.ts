66e4295343311ecfad463503dcfe04a94c958462
Manmath Kapale <manmath.kapale@EXXATSYSTEMS.COM>
2020-08-09 21:09:13 +0530

    Template added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmpUtilityService } from './../../services/emp-utility.service';
 
 @Component({
   selector: 'app-home',
@@ -7,22 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  studentList = [
-    { firstName: 'Anna', lastName: 'Strong', src: 'assets/images/woodland.jpeg', plCount: '2' },
-    { firstName: 'Rough', lastName: 'Strong', src: 'assets/images/GD.jpg', plCount: '3' },
-    { firstName: 'Lanna', lastName: 'Neil', src: 'https://material.angular.io/assets/img/examples/shiba2.jpg', plCount: '40' },
-    { firstName: 'Sanna', lastName: 'Strong', src: 'assets/images/woodland.jpeg', plCount: '5' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'assets/images/GD.jpg', plCount: '10' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'assets/images/woodland.jpeg', plCount: '10' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'assets/images/GD.jpg', plCount: '10' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'https://material.angular.io/assets/img/examples/shiba2.jpg', plCount: '10' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'assets/images/woodland.jpeg', plCount: '10' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'assets/images/GD.jpg', plCount: '10' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'assets/images/woodland.jpeg', plCount: '10' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'assets/images/GD.jpg', plCount: '10' },
-    { firstName: 'Anna', lastName: 'Strong', src: 'https://material.angular.io/assets/img/examples/shiba2.jpg', plCount: '10' }
-  ];
-  constructor() { }
+  
+  constructor(public empUtilityService: EmpUtilityService) { }
 
   ngOnInit(): void {
   }
--- src/app/components/shoutOut-template/shout-out-template/shout-out-template.component.ts
+++ src/app/components/shoutOut-template/shout-out-template/shout-out-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmpUtilityService } from 'src/app/services/emp-utility.service';
 
 @Component({
   selector: 'app-shout-out-template',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoutOutTemplateComponent implements OnInit {
 
-  constructor() { }
+  constructor(public empUtilityService: EmpUtilityService) { }
 
   ngOnInit(): void {
   }
